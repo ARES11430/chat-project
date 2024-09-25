bd3e70ab78684393f43b9ff473799f92d1bf0400
Amin Ghasemi <amin11.ghasemi@gmail.com>
2024-09-25 16:13:04 +0330

    add tanstack query to app

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,11 +2,27 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/Routes';
 
+const client = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 0,
+			gcTime: 0,
+			retry: false,
+			refetchOnWindowFocus: false,
+			refetchOnReconnect: false,
+			refetchOnMount: false
+		}
+	}
+});
+
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<QueryClientProvider client={client}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
 	</StrictMode>
 );
