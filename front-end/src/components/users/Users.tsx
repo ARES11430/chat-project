ca005ea3ccefaa030a80af1b12aba34f6d1c88e5
Amin Ghasemi <amin11.ghasemi@gmail.com>
2024-09-25 20:13:03 +0330

    styles for users

--- front-end/src/components/users/Users.tsx
+++ front-end/src/components/users/Users.tsx
@@ -1,3 +1,5 @@
+import { FaHome } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
 import useUsers from '../../hooks/useUsers';
 import { showErrorToast } from '../../utils/toastUtil';
 import Loading from '../Loading';
@@ -23,11 +25,24 @@ const Users = () => {
 		<>
 			{emptyError && <p className='font-bold text-center mt-5'>{emptyError}</p>}
 			{data && (
-				<ul>
-					{data?.data.users.map((user) => (
-						<li key={user._id}>{user.userName}</li>
-					))}
-				</ul>
+				<div
+					className={
+						'bg-base-200 items-center block relative pt-6 w-[30%] md:w-[25%] lg:w-[20%] 2xl:w-[12%]'
+					}
+				>
+					<NavLink to='/'>
+						<div className='flex items-center pr-3'>
+							<FaHome className='text-xl md:text-3xl ml-3' />
+							<h1 className={'font-bold text-2xl opacity-100 w-auto'}>خانه</h1>
+						</div>
+					</NavLink>
+					<h2 className={'font-bold text-3xl pr-3 mt-24'}>لیست کاربران</h2>
+					<ul className='flex flex-col gap-3 mt-7 pr-3'>
+						{data?.data.users.map((user) => (
+							<li key={user._id}>{user.userName}</li>
+						))}
+					</ul>
+				</div>
 			)}
 		</>
 	);
--- front-end/src/pages/ChatRoomPage.tsx
+++ front-end/src/pages/ChatRoomPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ChatRoomPage = () => {
-	return <div>ChatRoomPage</div>;
+	return <div className='bg-base-100 min-h-screen'>ChatRoomPage</div>;
 };
 
 export default ChatRoomPage;
