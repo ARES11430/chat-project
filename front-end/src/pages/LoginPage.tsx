06df8e60265fa5d4d658195fc4851a19d1ecc846
Amin Ghasemi <amin11.ghasemi@gmail.com>
2024-09-25 16:02:36 +0330

    create template for login and register

--- front-end/src/pages/LoginPage.tsx
+++ front-end/src/pages/LoginPage.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import LoginForm from '../components/auth/LoginForm';
 
 const LoginPage = () => {
-	return <div>LoginPage</div>;
+	return (
+		<div className='hero bg-base-100 min-h-screen'>
+			<div className='hero-content flex-col lg:flex-row-reverse lg:gap-20 gap-10 w-[90%] lg:w-[80%]'>
+				<div className='lg:w-[50%] w-full flex flex-col justify-center items-center text-center'>
+					<h1 className='text-3xl lg:text-5xl font-bold'>همین حالا وارد شوید!</h1>
+					<p className='py-4 lg:py-6'>لطفا به حساب کاربری خود وارد شوید</p>
+					<Link to='/register' className='hover:underline'>
+						حساب کاربری ندارید؟ برای ساخت حساب کاربری کلیک کنید!
+					</Link>
+				</div>
+				<LoginForm />
+			</div>
+		</div>
+	);
 };
 
 export default LoginPage;
--- front-end/src/pages/RegisterPage.tsx
+++ front-end/src/pages/RegisterPage.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import RegisterForm from '../components/auth/RegisterForm';
 
 const RegisterPage = () => {
-	return <div>RegisterPage</div>;
+	return (
+		<div className='hero bg-base-100 min-h-screen'>
+			<div className='hero-content flex-col lg:flex-row-reverse lg:gap-20 gap-10 w-[90%] lg:w-[80%]'>
+				<div className='lg:w-[50%] w-full flex flex-col justify-center items-center text-center'>
+					<h1 className='text-3xl lg:text-5xl font-bold'>همین حالا ثبت نام کنید!</h1>
+					<Link className='py-4 lg:py-6 hover:underline' to='/login'>
+						حساب کاربری دارید؟ به حساب کاربری خود وارد شوید
+					</Link>
+				</div>
+				<RegisterForm />
+			</div>
+		</div>
+	);
 };
 
 export default RegisterPage;
