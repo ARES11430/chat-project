68db181bdcb18b9407415822875944756e5e3178
Amin Ghasemi <amin11.ghasemi@gmail.com>
2024-09-25 19:50:43 +0330

    create hook for getting list of users

---
+++ front-end/src/hooks/useUsers.ts
@@ -0,0 +1,15 @@
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import { UserResponse } from '../models/users';
+import { getUsersService } from '../services/userServer';
+
+const useUsers = () => {
+	return useQuery<UserResponse, Error>({
+		queryKey: ['users'],
+		queryFn: () => {
+			return getUsersService.get();
+		},
+		placeholderData: keepPreviousData
+	});
+};
+
+export default useUsers;
