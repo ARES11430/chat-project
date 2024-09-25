4f1f16634837f8fc34162dfd599727e785fee0a0
Amin Ghasemi <amin11.ghasemi@gmail.com>
2024-09-25 16:32:47 +0330

    add toaster to main page

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'react-hot-toast';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/Routes';
 
@@ -24,5 +25,6 @@ createRoot(document.getElementById('root')!).render(
 		<QueryClientProvider client={client}>
 			<RouterProvider router={router} />
 		</QueryClientProvider>
+		<Toaster position='top-left' />
 	</StrictMode>
 );
