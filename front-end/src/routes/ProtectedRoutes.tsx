55c66c942a9fd74a8507c72d3a8c69e5644987f2
Amin Ghasemi <amin11.ghasemi@gmail.com>
2024-09-25 18:21:25 +0330

    create protected routes

---
+++ front-end/src/routes/ProtectedRoutes.tsx
@@ -0,0 +1,22 @@
+import { ReactElement } from 'react';
+import { Navigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+
+interface ProtectedRouteProps {
+	element: ReactElement;
+}
+
+const ProtectedRoutes = ({ element }: ProtectedRouteProps) => {
+	const { isLoggedIn } = useAuth();
+
+	if (!isLoggedIn) {
+		return element;
+	}
+
+	// if user is logged in navigate to chat page
+	if (isLoggedIn) {
+		return <Navigate to='/chat' replace />;
+	}
+};
+
+export default ProtectedRoutes;
