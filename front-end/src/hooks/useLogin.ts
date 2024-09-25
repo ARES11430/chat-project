6bc709ddbbea9112b64dba94e6096cbecfa26ba0
Amin Ghasemi <amin11.ghasemi@gmail.com>
2024-09-25 16:21:44 +0330

    create custom hook for login user

---
+++ front-end/src/hooks/useLogin.ts
@@ -0,0 +1,39 @@
+import toast from 'react-hot-toast';
+import { loginService } from '../services/authService';
+import { useAuthStore } from '../stores/authStore';
+import { Login, LoginResponse } from '../models/auth';
+import { useMutation } from '@tanstack/react-query';
+
+const useLogin = (navigate: (path: string) => void) => {
+	const setIsLoggedIn = useAuthStore((s) => s.setIsLoggedIn);
+	const setUserName = useAuthStore((s) => s.setUserName);
+	const setName = useAuthStore((s) => s.setName);
+	const setLastName = useAuthStore((s) => s.setLastName);
+
+	return useMutation<LoginResponse, Error, Login>({
+		mutationFn: (data: Login) => {
+			return loginService.post(data);
+		},
+		onSuccess: ({ data, message }) => {
+			setTimeout(() => {
+				setIsLoggedIn(true);
+				setUserName(data.userName);
+				setName(data.name);
+				setLastName(data.lastName);
+
+				if (message) {
+					navigate('/chat'); // * go to chat page after login
+					setTimeout(() => {
+						toast.success('با موفقیت وارد شدید', { duration: 2000 });
+					}, 500);
+				}
+			}, 2000); // 2-second delay before setting user info and navigating
+		},
+		onError: (error: any) => {
+			const errorMessage = error.response.data.error;
+			//showErrorToast(errorMessage);
+		}
+	});
+};
+
+export default useLogin;
