ce77e76f202b19949376af72aa4ca7e7dbc3c16c
Amin Ghasemi <amin11.ghasemi@gmail.com>
2024-09-25 19:07:35 +0330

    create register user service

--- front-end/src/models/auth.ts
+++ front-end/src/models/auth.ts
@@ -11,3 +11,22 @@ export interface LoginResponse {
 		lastName: string;
 	};
 }
+
+export interface RegisterUser {
+	name: string;
+	lastName: string;
+	userName: string;
+	password: string;
+}
+
+export interface RegisterUserResponse {
+	message: string;
+	data: {
+		user: {
+			_id: string;
+			name: string;
+			lastName: string;
+			userName: string;
+		};
+	};
+}
--- front-end/src/services/authService.ts
+++ front-end/src/services/authService.ts
@@ -1,4 +1,8 @@
-import { Login, LoginResponse } from '../models/auth';
+import { Login, LoginResponse, RegisterUser, RegisterUserResponse } from '../models/auth';
 import APIClient from './api-client';
+
+export const registerUserService = new APIClient<RegisterUser, RegisterUserResponse>(
+	'/auth/create-user'
+);
 
 export const loginService = new APIClient<Login, LoginResponse>('/auth/login');
