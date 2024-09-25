031040e5b2f34dfab8965af20b9719237a0a1b2f
Amin Ghasemi <amin11.ghasemi@gmail.com>
2024-09-25 17:07:27 +0330

    create actual login form
    actual login here

--- front-end/src/components/auth/LoginForm.tsx
+++ front-end/src/components/auth/LoginForm.tsx
@@ -1,7 +1,99 @@
-import React from 'react';
+import { useForm } from 'react-hook-form';
+import { BsFillKeyFill } from 'react-icons/bs';
+import { MdEmail } from 'react-icons/md';
+import { Link, useNavigate } from 'react-router-dom';
+
+// * components
+import Loading from '../Loading';
+import Spinner from '../Spinner';
+
+// * validation
+import { zodResolver } from '@hookform/resolvers/zod';
+import { loginSchema } from '../../validations/authSchema';
+
+// * hooks
+import useLogin from '../../hooks/useLogin';
+
+import { z } from 'zod';
+
+type LoginSchema = z.infer<typeof loginSchema>;
+
+const loginDefaultValues: LoginSchema = {
+	userName: '',
+	password: ''
+};
 
 const LoginForm = () => {
-	return <div>LoginForm</div>;
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitting }
+	} = useForm<LoginSchema>({
+		mode: 'all',
+		resolver: zodResolver(loginSchema),
+		defaultValues: loginDefaultValues
+	});
+
+	const navigate = useNavigate();
+
+	const { mutate, isPending, isSuccess } = useLogin(navigate);
+
+	const onSubmit = handleSubmit((data) => {
+		mutate(data);
+	});
+
+	if (isSuccess) return <Loading />;
+
+	return (
+		<div className='card bg-base-300 lg:w-[50%] w-full shadow-2xl'>
+			<form className='card-body' onSubmit={onSubmit}>
+				<div className='form-control'>
+					<label className='label text-xs font-bold'>
+						<span className='label-text text-xs'>نام کاربری</span>
+					</label>
+					<div className='input input-bordered flex items-center gap-2'>
+						<MdEmail className='text-lg' />
+						<input
+							type='text'
+							className='w-full small-placeholder text-xs xl:text-base'
+							placeholder='نام کاربری خود را وارد کنید'
+							{...register('userName')}
+						/>
+					</div>
+					{errors.userName && (
+						<p className='text-red-500 text-xs mt-2 ps-1'>{errors.userName.message}</p>
+					)}
+				</div>
+				<div className='form-control'>
+					<label className='label text-xs font-bold'>
+						<span className='label-text text-xs'>کلمه عبور</span>
+					</label>
+					<div className='input input-bordered flex items-center gap-2'>
+						<BsFillKeyFill className='text-lg' />
+						<input
+							type='password'
+							className='w-full small-placeholder text-xs xl:text-base'
+							placeholder='کلمه عبور خود را وارد کنید'
+							{...register('password')}
+						/>
+					</div>
+					{errors.password && (
+						<p className='text-red-500 text-xs mt-2 ps-1'>{errors.password.message}</p>
+					)}
+					<label className='label text-xs font-bold mt-5'>
+						<Link to='/register' className='label-text-alt link link-hover'>
+							قبلا ثبت نام کرده اید؟
+						</Link>
+					</label>
+				</div>
+				<div className='form-control'>
+					<button type='submit' className='btn btn-primary' disabled={isSubmitting || isPending}>
+						{isSubmitting || isPending ? <Spinner /> : 'ورود'}
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default LoginForm;
