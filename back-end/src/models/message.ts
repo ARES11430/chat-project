42463cb2ae7b8492331279eb047e9eaf2979fbe6
Amin Ghasemi <a.ghasemi@amnafzar.ir>
2024-11-11 09:22:36 +0330

    Refactor: change all the Models to support Class and objects

--- back-end/src/models/message.ts
+++ back-end/src/models/message.ts
@@ -1,37 +1,45 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema, Document, Model } from 'mongoose';
 import { IUser } from './user';
 
 export type IMessage = {
-	id: string;
-	user: IUser;
-	message: string;
-	timestamp: Date;
+  id: string;
+  user: IUser;
+  message: string;
+  timestamp: Date;
 };
 
-const messageSchema = new mongoose.Schema<IMessage>({
-	id: {
-		type: String,
-		required: [true, 'Message is required']
-	},
-	user: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User',
-		required: true
-	},
-	message: {
-		type: String,
-		required: [true, 'Message is required'],
-		trim: true,
-		minlength: [1, 'Message should be at least 1 character long'],
-		maxlength: [1000, 'Message should be at most 1000 characters long']
-	},
-	timestamp: {
-		type: Date,
-		default: Date.now // automatically set the timestamp
-	}
-});
+class MessageModel {
+  private schema: Schema<IMessage>;
 
-// Create a Message model from the schema
-const Message = mongoose.model<IMessage>('Message', messageSchema);
+  constructor() {
+    this.schema = new Schema<IMessage>({
+      id: {
+        type: String,
+        required: [true, 'Message ID is required'],
+      },
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'User is required'],
+      },
+      message: {
+        type: String,
+        required: [true, 'Message content is required'],
+        trim: true,
+        minlength: [1, 'Message should be at least 1 character long'],
+        maxlength: [1000, 'Message should be at most 1000 characters long'],
+      },
+      timestamp: {
+        type: Date,
+        default: Date.now,
+      },
+    });
+  }
 
+  getModel(): Model<IMessage> {
+    return mongoose.model<IMessage>('Message', this.schema);
+  }
+}
+
+const Message = new MessageModel().getModel();
 export { Message };
--- back-end/src/models/user.ts
+++ back-end/src/models/user.ts
@@ -1,72 +1,91 @@
-import dotenv from 'dotenv';
+import mongoose, { Schema, Document, Model } from 'mongoose';
 import jwt from 'jsonwebtoken';
-import mongoose from 'mongoose';
 
-export type IUser = {
-	name: string;
-	lastName: string;
-	userName: string;
-	password: string;
-	refreshToken?: string;
-	generateAuthToken(): string;
-	generateRefreshToken(): string;
-};
-
-dotenv.config();
-
+// Environment configuration
 const JWT_SECRET = process.env.ACCESS_TOKEN_SECRET;
 const REFRESH_SECRET = process.env.REFRESH_TOKEN_SECRET;
 
-const userSchema = new mongoose.Schema<IUser>(
-	{
-		name: {
-			type: String,
-			trim: true,
-			required: [true, 'property name is required'],
-			minlenght: [3, 'property name should be at least 3 charachters'],
-			maxlenght: [55, 'property name should be at most 55 charachters']
-		},
-		lastName: {
-			type: String,
-			trim: true,
-			required: [true, 'property name is required'],
-			minlenght: [3, 'property name should be at least 3 charachters'],
-			maxlenght: [55, 'property name should be at most 55 charachters']
-		},
-		userName: {
-			type: String,
-			trim: true,
-			required: [true, 'property name is required'],
-			unique: true
-		},
-		password: {
-			type: String,
-			trim: true,
-			required: [true, 'property name is required'],
-			minlenght: [6, 'property name should be at least 6 charachters'],
-			maxlenght: [1024, 'property name should be at most 1024 charachters']
-		},
-		refreshToken: {
-			type: String
-		}
-	},
-	{ timestamps: true }
-);
+export interface IUser extends Document {
+  name: string;
+  lastName: string;
+  userName: string;
+  password: string;
+  refreshToken?: string;
+  generateAuthToken(): string;
+  generateRefreshToken(): string;
+}
 
-userSchema.methods.generateAuthToken = function () {
-	if (JWT_SECRET) {
-		return jwt.sign({ _id: this._id, userName: this.userName }, JWT_SECRET, {
-			expiresIn: '10m'
-		});
-	} else return 'invalid Token';
-};
+class UserModel {
+  private schema: Schema<IUser>;
 
-userSchema.methods.generateRefreshToken = function () {
-	if (REFRESH_SECRET) {
-		return jwt.sign({ _id: this._id }, REFRESH_SECRET);
-	} else return 'invalid Token';
-};
+  constructor() {
+    this.schema = new Schema<IUser>(
+      {
+        name: {
+          type: String,
+          trim: true,
+          required: [true, 'property name is required'],
+          minlength: [3, 'property name should be at least 3 characters'],
+          maxlength: [55, 'property name should be at most 55 characters'],
+        },
+        lastName: {
+          type: String,
+          trim: true,
+          required: [true, 'property lastName is required'],
+          minlength: [3, 'property lastName should be at least 3 characters'],
+          maxlength: [55, 'property lastName should be at most 55 characters'],
+        },
+        userName: {
+          type: String,
+          trim: true,
+          required: [true, 'property userName is required'],
+          unique: true,
+        },
+        password: {
+          type: String,
+          trim: true,
+          required: [true, 'property password is required'],
+          minlength: [6, 'property password should be at least 6 characters'],
+          maxlength: [
+            1024,
+            'property password should be at most 1024 characters',
+          ],
+        },
+        refreshToken: {
+          type: String,
+        },
+      },
+      { timestamps: true }
+    );
 
-const User = mongoose.model('User', userSchema);
+    // Attach methods to the schema
+    this.schema.methods.generateAuthToken = this.generateAuthToken;
+    this.schema.methods.generateRefreshToken = this.generateRefreshToken;
+  }
 
+  // Methods to be used on Mongoose documents
+  generateAuthToken(this: IUser) {
+    if (JWT_SECRET) {
+      return jwt.sign({ _id: this._id, userName: this.userName }, JWT_SECRET, {
+        expiresIn: '10m',
+      });
+    } else {
+      throw new Error('Invalid Token Secret');
+    }
+  }
+
+  generateRefreshToken(this: IUser) {
+    if (REFRESH_SECRET) {
+      return jwt.sign({ _id: this._id }, REFRESH_SECRET);
+    } else {
+      throw new Error('Invalid Refresh Token Secret');
+    }
+  }
+
+  getModel(): Model<IUser> {
+    return mongoose.model<IUser>('User', this.schema);
+  }
+}
+
+const User = new UserModel().getModel();
 export { User };
